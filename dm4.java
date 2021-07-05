c2d7b9d478d2454edea84b84d84d790f439e555a
遠藤冬羽 <endotowa@wlan-napt-001.u-aizu.ac.jp>
2021-07-05 19:08:00 +0900

    Greetings added

--- dm4.java
+++ dm4.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class dm4 {
     public static void main(String[] args){
-    System.out.println("Rolling the dice...");
+
     int a,b,c;
 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    System.out.print("> ");
+    String name = scan.nextLine();
+    System.out.println("Hello, "+name+"!");
+
+    System.out.println("Rolling the dice...");
     a = (int)(Math.random() * 6) + 1;
     b = (int)(Math.random() * 6) + 1;
     System.out.println("Die 1:" +a);
