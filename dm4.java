2865162881c17147e6fdd52296a08ee3a0b741a0
遠藤冬羽 <endotowa@wlan-napt-001.u-aizu.ac.jp>
2021-07-05 19:11:27 +0900

    Won/lost message added

--- dm4.java
+++ dm4.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 public class dm4 {
     public static void main(String[] args){
-    System.out.println("Rolling the dice...");
+
     int a,b,c;
 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    System.out.print("> ");
+    String name = scan.nextLine();
+    System.out.println("Hello, "+name+"!");
+
+    System.out.println("Rolling the dice...");
     a = (int)(Math.random() * 6) + 1;
     b = (int)(Math.random() * 6) + 1;
     System.out.println("Die 1:" +a);
     System.out.println("Die 2:" +b);
     c=a+b;
     System.out.println("Total value:" +c);
+    if(c>7){
+        System.out.println("You won!");
+    }
+    else {
+        System.out.println("You lost");
+    }
     }
     }
     
